9698ae8eefbfb72cc86a131bf328d6be18503c9b
HUANG-mle <abris2649@gmail.com>
2026-03-30 20:31:09 +0700

    Create Day2_strings.py

---
+++ Day2_strings.py
@@ -0,0 +1,14 @@
+# Topic: Variables & Data Types
+# Date: 30 March 2026
+# Source: Self-study
+
+name = "Huang"
+age = int("14")
+height = float("1.5")
+
+print(f"Hello, my name is {name}")
+print(f"My age is {age}")
+print(f"My height is {height}")
+
+graduated = True
+print(graduated)
