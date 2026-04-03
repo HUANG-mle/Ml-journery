56cd13378aa0e2142ba79827912bfd06a04771c0
HUANG-mle <abris2649@gmail.com>
2026-04-03 18:55:45 +0700

    Create Day6_def.py

---
+++ Day6_def.py
@@ -0,0 +1,99 @@
+LEARNING JOURNAL — DAY 5
+Date           : April 3, 2026
+Study duration : 30 minutes
+Main topic     : Definitions
+
+1. Concepts I learned today
+Functions
+
+2. New things I found out
+It turns out functions can be used for multiplication.
+
+3. The code I wrote today
+
+Python
+def hitung_luas(panjang, lebar):
+	luas = panjang*lebar
+	print(f"luas nya adalah : {luas}")
+hitung_luas(7, 8)
+4. Errors I encountered & how I fixed them
+When I was setting up the length and width, the program didn't run.
+ It turns out I had to change the def name to hitung_luas and update the function call to hitung_luas as well.
+
+5. What still confuses me
+Nothing
+
+6. My understanding score today (1-10)
+8/10
+
+
+
+JURNAL BELAJAR — DAY 5
+
+======================
+
+
+
+Tanggal : 03 -april -2026
+
+Durasi belajar : 30 menit
+
+Materi utama : definisi
+
+
+
+1. Konsep yang gw pelajari hari ini
+
+fungsi
+
+
+
+
+
+2. Hal baru yang gw tau
+
+ternyata fungsi bisa di pake buat perkalian
+
+
+
+
+
+3. Kode yang gw tulis hari ini
+
+
+
+def hitung_luas(panjang, lebar):
+
+luas = panjang*lebar
+
+print(f"luas nya adalah : {luas}")
+
+hitung_luas(7, 8)
+
+
+
+
+
+4. Error yang gw temuin & cara gw fixnya
+
+pas mau bikin panjang lebar nya dan progam nya gak jalan
+
+ternyata harus nama nya atau def diganti jadi
+
+hitung luas dan nama fungsinya di ganti juga jadi hitung luas
+
+
+
+5. Yang masih bikin gw bingung
+
+gak ada
+
+
+
+
+
+6. Skor pemahaman gw hari ini (1-10)
+
+8/10
+
+
