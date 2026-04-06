bd8ef45eb9ad709008b1f152657168a462d60cf7
HUANG-mle <abris2649@gmail.com>
2026-04-06 18:38:17 +0700

    Create Day6_dictionaries.py

---
+++ Day6_dictionaries.py
@@ -0,0 +1,67 @@
+LEARNING JOURNAL — DAY 7
+Date : April 6, 2026
+Duration : 90 minutes
+Main Topic : Dictionaries
+1. Concepts I Learned Today
+Dictionaries and keys.
+2. New Insights
+I discovered that I can add or update data even after the profile has been created, as long as it’s done before the output.
+3. Code I Wrote Today
+
+profile = {
+    "name" : "Huang",
+    "age" : 17,
+    "city" : "Surabaya",
+}
+
+# Updating and adding new data
+profile["age"] = 18
+profile["hobby"] = "coding"
+
+for key in profile:
+    print(f"{key} : {profile[key]}")
+  
+  4. Errors Encountered & Solutions
+I struggled with adding new data to the dictionary. I realized that the update or addition must be written before the print/output section of the code to take effect.
+5. What Still Confuses Me
+I’m still a bit unsure about how to use user input() to assign values to the dictionary variables.
+6. Understanding Score
+7.5/10
+JURNAL BELAJAR — DAY 7
+======================
+
+Tanggal        : 6 - 4 - 2026
+Durasi belajar : 90 menit
+Materi utama   : dictionary 
+
+1. Konsep yang gw pelajari hari ini
+     dictionary and key
+
+
+2. Hal baru yang gw tau
+ternyata bisa nambahin atau update setelah bikin profil nya atau 
+sebelum outputnya 
+
+3. Kode yang gw tulis hari ini
+profil = {
+	"nama" : "Huang" ,
+	"umur" : 17 ,
+	"kota" : "surabaya" ,
+}
+profil["umur"] = 18
+profil["hobi"] = "coding"
+for key in profil :
+	print(f"{key} : {profil[key]}")
+
+
+4. Error yang gw temuin & cara gw fixnya
+yaitu pas mau bikin atau nambahin data nya lagi ke dictionary gw 
+ternyata sebelum outputnya harus di bikin atau updatenya di situ
+
+5. Yang masih bikin gw bingung
+ gimana kalok pake input ke variabel nya 
+
+
+6. Skor pemahaman gw hari ini (1-10)
+7,5/10
+
